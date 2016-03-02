fc048b6540cbb83ac77ce4e340502da11ee892cd
StefanLederer <stefan.lederer18@gmail.com>
2016-03-02 14:40:23 +0100

    [FE] created buttonClick with popUp

--- web/js/buttonClick.js
+++ web/js/buttonClick.js
@@ -3,21 +3,14 @@
  */
 
 $(document).ready(function() {
-    var value;
-    $('.button').click(function (e) {
-        e.preventDefault();
-        value = $(this).val();
-        $('.button-form').submit();
-    });
-    $('.button-form').submit(function (e) {
-        e.preventDefault();
+    $('.button').click(function(event) {
 
-        console.log(value);
-        var url = "/abgabe";
-        $.post( url, {
-            button: value
-        }).fail(function() {
-            console.log( "error" );
-        });
+        console.log('clicked');
+        $('.popUp').css("visibility","visible");
+        setTimeout(function() {
+            $('.popUp').css("visibility","hidden");
+        },4000);
+
+        event.preventDefault();
     });
 });

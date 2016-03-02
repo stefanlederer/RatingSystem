9a2af4ad089587992f06b3a5ef350c95922b20da
StefanLederer <stefan.lederer18@gmail.com>
2016-03-02 14:46:53 +0100

    [FE]  proof if query is actual

---
+++ web/js/ajax.js
@@ -0,0 +1,19 @@
+/**
+ * Created by stefan on 02.03.16.
+ */
+setInterval(function() {
+    query();
+}, 5000); //one hour = 3600000
+
+function query() {
+    $.ajax({
+        type: "POST",
+        url: "/",
+        data: "FunctionName=ajax",
+        success: successed()
+    })
+}
+
+function successed() {
+    console.log('ajax');
+}

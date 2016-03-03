fcd2aacd75c212c4fb284737a62682b8c5ed6bd8
StefanLederer <stefan.lederer18@gmail.com>
2016-03-03 09:34:23 +0100

    [FE] [BE] fixed button click

--- web/js/buttonClick.js
+++ web/js/buttonClick.js
@@ -3,28 +3,36 @@
  */
 
 $(document).ready(function () {
-    $(".button-form").submit(function (event) {
+    var clicked = false;
+    var elem = '';
+    $('.button').click(function () {
+        clicked = true;
+        elem = this;
+        $('.button-form').submit();
+    });
+    $(".button-form").submit(function (e) {
+        e.preventDefault();
+        if (clicked) {
+            console.log('clicked');
 
-        console.log('clicked');
+            $('.button').prop("disabled", true);
+            $('.popUp').css("visibility", "visible");
+            setTimeout(function () {
+                $('.button').prop("disabled", false);
+                $('.popUp').css("visibility", "hidden");
+            }, 3000);
 
-        $('.button').prop("disabled", true);
-        $('.popUp').css("visibility","visible");
-        setTimeout(function () {
-            $('.button').prop("disabled", false);
-            $('.popUp').css("visibility","hidden");
-        }, 3000);
-
-        var buttontype = $('.button').val();
-        console.log(buttontype);
-        $.ajax({
-            type: "POST",
-            url: "/",
-            data: {
-                button: buttontype
-            },
-            success: console.log("success")
-        });
-
-        event.preventDefault();
+            var buttontype = $(elem).val();
+            console.log(buttontype);
+            $.ajax({
+                type: "POST",
+                url: "/",
+                data: {
+                    button: buttontype
+                },
+                success: console.log("success")
+            });
+            clicked = false;
+        }
     });
 });

92dfcf15e9492918bbfe5cd3938ee57edc729d9e
StefanLederer <stefan.lederer18@gmail.com>
2016-04-29 22:21:59 +0200

    [BE] updated ajax

--- web/js/ajax.js
+++ web/js/ajax.js
@@ -2,18 +2,5 @@
  * Created by stefan on 02.03.16.
  */
 setInterval(function() {
-    query();
-}, 5000); //one hour = 3600000
-
-function query() {
-    $.ajax({
-        type: "POST",
-        url: "/",
-        data: "FunctionName=ajax",
-        success: successed()
-    })
-}
-
-function successed() {
-    console.log('ajax');
-}
+    location.reload();
+}, 6000);

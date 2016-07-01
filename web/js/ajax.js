615778a7622abd5258aec46f2cf1878c6041037a
StefanLederer <stefan.lederer18@gmail.com>
2016-07-01 21:35:31 +0200

    [FE] fixed updating bewertung.html.twig

--- web/js/ajax.js
+++ web/js/ajax.js
@@ -1,6 +1,30 @@
 /**
  * Created by stefan on 02.03.16.
  */
-setInterval(function() {
-    location.reload();
-}, 600000); 
+// setInterval(function() {
+// location.reload();
+// $.ajax({
+$(document).ready(function () {
+    // update();
+    // function update() {
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "/",
+    //         success: console.log("success")
+    //     }).then(function () {
+    //         setTimeout(update, 10000);
+    //     });
+    // }
+    setTimeout(function(){
+        location = '/'
+    },10000);
+});
+// $.post("/");
+//     type: "GET",
+//     url: "/",
+//     success: console.log("success")
+// });
+// }, 10000);
+//setInterval(function() {
+//     location.reload();
+// }, 10000);
